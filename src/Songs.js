593035590be1a5c0c8ce160c485c40d4534d9f15
jacheampong <jacheampong@gmail.com>
2020-12-07 20:27:38 -0500

    muse react frontend

---
+++ src/Songs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card, Button} from 'semantic-ui-react';
+export default function Songs(props){
+  const songs = props.songs.map((song) => {
+    return (
+        <Card key={song.id}>
+          <Card.Content>
+            <Card.Header>{song.title}</Card.Header>
+            <Card.Description>{song.artist}</Card.Description>
+            <Card.Description>{song.album}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Button>Delete Song</Button>
+            <Button>Edit Song</Button>
+          </Card.Content>
+        </Card>
+        )
+  })
+  return (
+      <Card.Group>
+        { songs }
+      </Card.Group>
+    )
+}
